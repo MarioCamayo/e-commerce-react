4662f35c059afdc6f8b02554a929731146f70080
mario camayo <mariocamayo17@gmail.com>
2024-09-20 01:50:28 -0500

    se modificó el componente SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
- import {useContext} from 'react'
- import {shopContext} from '../context/ShopContext'
+//  import {useContext} from 'react'
+//  import {shopContext} from '../context/ShopContext'
  
  const SearchBar = ()=>{
-   const {search, setSearch, showSearch, setShowSearch}=  useContext(shopContext)
+  //  const {search, setSearch, showSearch, setShowSearch}=  useContext(shopContext)
   return(
       <h1>hola</h1>
   )
